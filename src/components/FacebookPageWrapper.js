60638ad3d2f21d0815122b2e2ec85e9c3afa6f59
CDProjects <caseydentprojects@gmail.com>
2026-06-16 08:39:27 +0300

    Facebook plugin next.js fix

--- src/components/FacebookPageWrapper.js
+++ src/components/FacebookPageWrapper.js
@@ -1,68 +1,65 @@
+"use client";
+
 import React, { useEffect, useRef } from 'react';
+import Script from 'next/script'; // Import Next.js Script component
 
 const FacebookPageWrapper = ({ fbPageUrl, tabs, width, height }) => {
   const containerRef = useRef(null);
 
-  useEffect(() => {
-    // 1. Function to parse the widget
-    const parseXFBML = () => {
-      if (window.FB && containerRef.current) {
-        window.FB.XFBML.parse(containerRef.current);
-      }
-    };
-
-    // 2. If the SDK is already loaded (from a previous page visit), just parse
-    if (window.FB) {
-      parseXFBML();
-      return;
-    }
-
-    // 3. If SDK is not loaded, load it now
-    if (!document.getElementById('facebook-jssdk')) {
-      window.fbAsyncInit = function() {
-        window.FB.init({
-          xfbml: true,
-          version: 'v20.0'
-        });
-        parseXFBML();
-      };
-
-      const script = document.createElement('script');
-      script.id = 'facebook-jssdk';
-      script.src = 'https://connect.facebook.net/en_GB/sdk.js';
-      script.async = true;
-      script.defer = true;
-      // REMOVED: script.crossOrigin = 'anonymous';  <-- This was causing the Blocked Error
-      
-      document.body.appendChild(script);
-    }
-  }, []); // Run on mount
-
-  // 4. Re-parse if props change (width/url)
-  useEffect(() => {
+  // Function to tell Facebook to render the widget
+  const parseXFBML = () => {
     if (window.FB && containerRef.current) {
       window.FB.XFBML.parse(containerRef.current);
     }
+  };
+
+  // Re-parse if the width changes (e.g. rotating a mobile phone)
+  useEffect(() => {
+    parseXFBML();
   }, [width, height, fbPageUrl]);
 
   return (
-    <div ref={containerRef} key={width} style={{ display: 'flex', justifyContent: 'center', minHeight: height + 'px' }}>
+    <>
+      {/* The Next.js way to safely load the Facebook SDK */}
+      <Script 
+        id="facebook-jssdk" 
+        src="https://connect.facebook.net/en_GB/sdk.js" 
+        strategy="lazyOnload" // Loads in the background so it doesn't slow down your site
+        onLoad={() => {
+          // Once the script downloads, initialize it and parse the widget
+          if (window.FB) {
+            window.FB.init({
+              xfbml: true,
+              version: 'v20.0'
+            });
+            parseXFBML();
+          }
+        }}
+      />
+
+      {/* The actual Facebook Widget HTML */}
       <div 
-        className="fb-page" 
-        data-href={fbPageUrl}
-        data-tabs={tabs}
-        data-width={width}
-        data-height={height}
-        data-small-header="false"
-        data-adapt-container-width="true"
-        data-hide-cover="false"
-        data-show-facepile="true"
+        ref={containerRef} 
+        key={width} 
+        style={{ display: 'flex', justifyContent: 'center', minHeight: height + 'px', width: '100%' }}
       >
-        <blockquote cite={fbPageUrl} className="fb-xfbml-parse-ignore">
-          <a href={fbPageUrl}>Old Town Shamrocks Porvoo</a>
-        </blockquote>
+        <div 
+          className="fb-page" 
+          data-href={fbPageUrl}
+          data-tabs={tabs}
+          data-width={width}
+          data-height={height}
+          data-small-header="false"
+          data-adapt-container-width="true"
+          data-hide-cover="false"
+          data-show-facepile="true"
+        >
+          <blockquote cite={fbPageUrl} className="fb-xfbml-parse-ignore">
+            <a href={fbPageUrl}>Old Town Shamrocks Porvoo</a>
+          </blockquote>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
